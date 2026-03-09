7224f876ee295022fb9658f23d20e9f3c69f135b
Hardy Fernandes <hardymtf@gmail.com>
2026-03-09 13:04:35 -0100

    first day writing java program

---
+++ src/week_1_java_fundamentals/day_2_first_java_program/HelloWorld.java
@@ -0,0 +1,10 @@
+package week_1_java_fundamentals.day_2_first_java_program;
+
+public class HelloWorld {
+    public static void main(String[] ars) {
+
+        System.out.println("Hello World"); // prints with new line
+        System.out.print("Hello "); //print without new line
+        System.out.print("Java!\n"); //\n = new line manually
+    }
+}
---
+++ src/week_1_java_fundamentals/day_2_first_java_program/breakitdown.md
@@ -0,0 +1,7 @@
+public: accessible from anywhere
+class: blueprint for your program
+HelloWorld: name of the class (must match filename)
+static: belongs to the class, not an object
+void: methods returns nothing
+main: starting point of the program
+String[] args: command-line arguments
