78f85ba5bb03d298ff8441bdd00418b97f97ac3f
Hardy Fernandes <hardymtf@gmail.com>
2026-03-09 05:58:32 -0100

    add how to setup java enviroment

---
+++ src/week_1_java_fundamentals/day_1_setup_java_enviroment/install&verify.bat
@@ -0,0 +1,12 @@
+# JDK - Java Development Kit (write $ compile code)
+# JVM  - Java Virtual Machine (runs the code)
+# JRE - Java Runtime Environment (runs compiled code)
+
+# Check if Java is installed
+java -version
+
+# Check compiler
+javac -version
+
+###
+Your Code (.java) → Compiler (javac) → Bytecode(.class) → JVM → Output
