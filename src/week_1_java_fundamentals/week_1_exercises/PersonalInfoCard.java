a4c68bcf1e5195c4408de5bd2c23b310ded6292d
Hardy Fernandes <hardymtf@gmail.com>
2026-03-11 05:27:39 -0100

    personal card info exercise

---
+++ src/week_1_java_fundamentals/week_1_exercises/PersonalInfoCard.java
@@ -0,0 +1,37 @@
+package week_1_java_fundamentals.week_1_exercises;
+
+// Create a program that:
+// 1. Asks for name, age, and city
+// 2. Prints a formatted info card like:
+// ====================================
+// Name: Alice
+// Age: 25
+// City:cNew York
+// ====================================
+
+import java.util.Scanner;
+
+public class PersonalInfoCard {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Personal Card information");
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+
+        System.out.println("How old are you?");
+        int age = scanner.nextInt();
+
+        System.out.println("What city you live?");
+        String city = scanner.nextLine();
+
+        System.out.println("=================");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("City: " + city);
+        System.out.println("=================");
+
+        scanner.close();
+    }
+}
