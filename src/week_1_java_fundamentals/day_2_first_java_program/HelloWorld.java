e76165f9f5b316ac3ca2b2e7ebb92738e8bf9db1
Hardy Fernandes <hardymtf@gmail.com>
2026-05-13 22:02:19 -0100

    style: add comments

--- src/week_1_java_fundamentals/day_2_first_java_program/HelloWorld.java
+++ src/week_1_java_fundamentals/day_2_first_java_program/HelloWorld.java
@@ -4,7 +4,17 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World"); // prints with new line
-        System.out.print("Hello "); //print without new line
-        System.out.print("Java!\n"); //\n = new line manually
+        System.out.print("Hello "); // print without new line
+        System.out.print("Java!\n"); // \n = new line manually
     }
+
+    /*
+     * public -> accessible from anywhere
+     * class -> blueprint for your program
+     * HelloWorld -> name of the class (must match filename)
+     * static -> belongs to the class, not an object
+     * void -> methods returns nothing
+     * main -> starting point of the program
+     * String[] args -> command-line arguments
+     */
 }
