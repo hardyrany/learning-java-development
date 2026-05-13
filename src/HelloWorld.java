e2b952d13c64f77cb668ac3320a1591b44db7c34
Hardy Fernandes <hardymtf@gmail.com>
2026-05-13 21:50:08 -0100

    style: comments in file Hello World

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,4 +3,11 @@ public class HelloWorld {
 
         System.out.println("Hello, World!");
     }
+
+    /*
+     * Key concepts:
+     * public class -> every JAVA file has a class
+     * main -> entry point of every JAVA program
+     * System.out.println -> output/prints to console
+     */
 }
