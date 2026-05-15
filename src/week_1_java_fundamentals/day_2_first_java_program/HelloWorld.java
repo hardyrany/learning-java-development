805368ed586000839d2e3d55d40654cd81dc319e
Hardy Fernandes <hardymtf@gmail.com>
2026-05-15 11:32:29 -0100

    docs: helloworld comments

--- src/week_1_java_fundamentals/day_2_first_java_program/HelloWorld.java
+++ src/week_1_java_fundamentals/day_2_first_java_program/HelloWorld.java
@@ -3,9 +3,14 @@ package week_1_java_fundamentals.day_2_first_java_program;
 public class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hello World"); // prints with new line
-        System.out.print("Hello "); // print without new line
-        System.out.print("Java!\n"); // \n = new line manually
+        // Prints text and moves to the next line
+        System.out.println("Hello World");
+
+        // Prints text without moving to a new line
+        System.out.print("Hello ");
+
+        // \n manually creates a new line
+        System.out.print("Java!\n"); 
     }
 
     /*
