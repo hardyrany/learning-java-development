520e5414e2e53a44f5e6553d288238964517db08
Hardy Fernandes <hardymtf@gmail.com>
2026-05-15 12:19:43 -0100

    docs: operators comments

--- src/week_1_java_fundamentals/day_4_operators/Operators.java
+++ src/week_1_java_fundamentals/day_4_operators/Operators.java
@@ -3,18 +3,40 @@ package week_1_java_fundamentals.day_4_operators;
 public class Operators {
     public static void main(String[] args) {
 
+        // declare two integer variables
         int a = 10, b = 3;
 
-        System.out.println("a: " + a);  // 10 value
-        System.out.println("b: " + b);  // 3 value
-        System.out.println("a + b: " + (a + b));    // 13 (addition)
-        System.out.println("a + b: " + (a - b));    // 7 (subtraction)
-        System.out.println("a * b: " + (a * b));    // 30 (multiplication)
-        System.out.println("a / b: " + (a / b));    // 3 (division - integer result!)
-        System.out.println("a % b: " + (a % b));    // 1 (modulus - remainder)
+        // print variable values
+        System.out.println("a: " + a);  // 10
+        System.out.println("b: " + b);  // 3
 
-        // Fix division to get decimal
+        // addition
+        // + adds two numbers
+        System.out.println("a + b: " + (a + b));    // 13
+        
+        // subtraction
+        // - subtracts secund number from first
+        System.out.println("a + b: " + (a - b));    // 7
+        
+        // multiplication
+        // * multiplies two numbers
+        System.out.println("a * b: " + (a * b));    // 30
+        
+        // division
+        // / divides numbers
+        // integer division removes decimal part
+        System.out.println("a / b: " + (a / b));    // 3
+        
+        // modulus
+        // % returns remainder after division
+        System.out.println("a % b: " + (a % b));    // 1
+
+        // TYPE CASTING
+        // convert a to double for decimal division
+        // fix division to get decimal
         double result = (double) a / b;
+
+        // prints decimal result
         System.out.println("result: " + result); // 3.3333...
     }
 }
