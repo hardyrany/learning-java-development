97838ef57141b833ca66a75a0c588df155d1b784
Hardy Fernandes <hardymtf@gmail.com>
2026-05-15 14:31:18 -0100

    docs: main commentary

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        
+        // printf is used for formatted output (like print with formatting options)
+        System.out.printf("Hello and welcome!\n");
 
+        // loop runs from 1 to 5 (inclusive)
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            
+            // prints current value of i in each iteration
             System.out.println("i = " + i);
         }
     }
