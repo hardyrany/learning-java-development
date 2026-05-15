e06a0646a971f8bd2ed867825f08859b7844dbb5
Hardy Fernandes <hardymtf@gmail.com>
2026-05-15 11:57:50 -0100

    docs: string comments

--- src/week_1_java_fundamentals/day_3_data_types/StringExample.java
+++ src/week_1_java_fundamentals/day_3_data_types/StringExample.java
@@ -3,17 +3,36 @@ package week_1_java_fundamentals.day_3_data_types;
 public class StringExample {
     public static void main(String[] args) {
 
+        // String stores text data
         String name = "Alice";
-        String greeting = "Hello, " + name + "!"; // concatenation
+
+        // String concatenation joins multiple strings together
+        String greeting = "Hello, " + name + "!";
 
         // Useful String methods
-        System.out.println(name.length());  // 5
-        System.out.println(name.toUpperCase()); // ALICE
-        System.out.println(name.toLowerCase()); // aline
-        System.out.println(name.charAt(0)); // A
+
+        // length() returns the number of characters
+        System.out.println(name.length());
+
+        // toUpperCase() converts all letters to uppercase
+        System.out.println(name.toUpperCase());
+
+        // to LowerCase() converts all letters to lowercase
+        System.out.println(name.toLowerCase());
+
+        // charAt(index) returns character at a specific position
+        System.out.println(name.charAt(0));
+
+        // contain() checks if text exists inside the string
         System.out.println(name.contains("lic"));
-        System.out.println(name.replace("A", "E")); // Elice
-        System.out.println(" hi".trim());   // hi (removes spaces)
-        System.out.println(greeting);   // Hello, Alice!
+
+        // replace() characters or words
+        System.out.println(name.replace("A", "E"));
+
+        // trim() removes spaces from beginning and end
+        System.out.println(" hi".trim());
+
+        // print final greeting
+        System.out.println(greeting);
     }
 }
