92cd12af7c9647546758fb5a76db4cb2142b5c28
Hardy Fernandes <hardymtf@gmail.com>
2026-05-15 14:39:00 -0100

    docs: datatypes comment

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,15 +1,26 @@
 public class DataTypes {
     public static void main(String[] args) {
 
-        // Primitive types
+        // PRIMITIVE DATA TYPES
+
+        // int stores whole value numbers
         int age = 25;
+
+        // double stores decimal numbers (more precise)
         double salary = 50000.50;
+
+        // char stores a single character (single quotes)
         char grade = 'A';
+
+        // boolean stores true or false values
         boolean isStudent = true;
 
-        // String (object type)
+        // NON-PRIMITIVE TYPE(OBJECT)
+
+        // String stores text (sequence of characters)
         String name = "John";
 
+        // OUTPUT VALUES
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
         System.out.println("Salary: " + salary);
