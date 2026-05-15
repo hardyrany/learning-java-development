c6956606098559eab724a05a9187c76319227d50
Hardy Fernandes <hardymtf@gmail.com>
2026-05-15 12:11:04 -0100

    docs: logical operators comments

--- src/week_1_java_fundamentals/day_4_operators/LogicalOps.java
+++ src/week_1_java_fundamentals/day_4_operators/LogicalOps.java
@@ -3,21 +3,46 @@ package week_1_java_fundamentals.day_4_operators;
 public class LogicalOps {
     public static void main(String[] args) {
 
+        // declare two integer variables
         int x = 10, y = 20;
+
+        // print variables
         System.out.println(x);
         System.out.println(y);
 
-        // Comparison (returns true/false)
-        System.out.println(x == y); // false (equal to)
-        System.out.println(x != y); // true (not equal)
-        System.out.println(x > y);  // false (greater than)
-        System.out.println(x < y);  // true (less than)
-        System.out.println(x >= y); // false (greater or equal)
-        System.out.println(x <= y); // true (less or equal)
+        // COMPARISON OPERATORS
+        // comparison operators return true or false
 
-        // Logical
-        System.out.println(x < y && x > 5); // true (AND - both must be true)
-        System.out.println(x > y || x > 5); // true (OR - one must be true)
-        System.out.println(!(x == y));  // true (NOT - reverses result)
+        // == check if values are equal
+        System.out.println(x == y); // false
+        
+        // != checks if values are not equal
+        System.out.println(x != y); // true
+        
+        // > checks if left value is greater
+        System.out.println(x > y);  // false
+
+        // < checks if left value is smaller
+        System.out.println(x < y);  // true
+
+        // >= checks if greater than or equal
+        System.out.println(x >= y); // false
+
+        // <= checks if less than or equal
+        System.out.println(x <= y); // true 
+
+        // LOGICAL OPERATORS
+
+        // && (AND)
+        // true only if both conditions are true
+        System.out.println(x < y && x > 5); // true
+
+        // || (OR)
+        // true if at least one condition is true
+        System.out.println(x > y || x > 5); // true
+
+        // ! (NOT)
+        // reverses the result
+        System.out.println(!(x == y));  // true
     }
 }
