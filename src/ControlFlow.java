f0ad5f4b0fa33919afbab56fbe578d303a10996b
Hardy Fernandes <hardymtf@gmail.com>
2026-05-15 14:46:15 -0100

    docs: control flow comment

--- src/ControlFlow.java
+++ src/ControlFlow.java
@@ -1,30 +1,45 @@
 public class ControlFlow {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
+        // variable to store integer value score
         int score = 75;
 
-        // If-else
+        // IF-ELSE STATEMENT (decision making)
+
+        // checks if score is 90 or more
         if (score >= 90) {
             System.out.println("Grade: A");
+
+            // checks if score is 75 or more
         } else if (score >= 75) {
             System.out.println("Grade: B");
+
+            // runs if none of the above conditions are true
         } else {
             System.out.println("Grade: C");
         }
 
         System.out.println();
 
-        // For loop
+        // FOR LOOP (repeats a block of code a fixed number of times)
+
+        // runs from 1 to 90
         for (int i = 1; i <= 90; i++) {
             System.out.println("Count: " + i);
         }
 
         System.out.println();
 
-        // While loop
+        // WHILE LOOP (repeats while condition is true)
+
+        // variable to store integer value x
         int x = 10;
+
+        // loop runs x until x becomes 0 or negative
         while (x > 0) {
             System.out.println(x);
+
+            // decrease x 3 each time
             x -= 3;
         }
     }
