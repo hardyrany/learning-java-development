e628fb4970facb561190570b5d91f47cdce62da2
Hardy Fernandes <hardymtf@gmail.com>
2026-05-19 19:10:35 -0100

    docs: temperature converter comment

--- src/week_1_java_fundamentals/week_1_exercises/TemperatureConverter.java
+++ src/week_1_java_fundamentals/week_1_exercises/TemperatureConverter.java
@@ -1,23 +1,39 @@
 package week_1_java_fundamentals.week_1_exercises;
 
+// PROGRAM:
 // Convert Celsius to Fahrenheit
 // Formula: F = (C x 9/5) + 32
 // Ask user to enter Celsius, print Fahrenheit result
 
-import java.util.Scanner;
+import java.util.Scanner;   // import Scanner class for user input
 
 public class TemperatureConverter {
     public static void main(String[] args) {
 
+        // SCANNER OBJECT:
+        // used to read keyword input
         Scanner scanner = new Scanner(System.in);
 
+        // PROGRAM TITLE
         System.out.println("Convert Celsius to Fahrenheit");
-        System.out.println("What is Celsius degree?");
-        int celsius = scanner.nextInt();
-        int fahrenheit = celsius * 9/5 + 32;
 
+        // USER INPUT SECTION
+
+        // ask user for Celsius temperature value
+        System.out.println("What is Celsius degree?");
+
+        // read integer temperature value
+        int celsius = scanner.nextInt();
+        
+        // CONVERSION FORMULA
+
+        // convert Celsius to Fahrenheit
+        int fahrenheit = (celsius * 9 / 5) + 32;
+
+        // print Fahrenheit temperature
         System.out.println("Fahrenheit is: " + fahrenheit);
 
+        // close scanner to free system resources
         scanner.close();
     }
 }
