db1e5929537d6cbdf96428d01345dc0d73841512
Hardy Fernandes <hardymtf@gmail.com>
2026-05-20 15:22:18 -0100

    docs: grader checker comment

--- src/week_2_control_flow/day_1_if_else_statements/GradeChecker.java
+++ src/week_2_control_flow/day_1_if_else_statements/GradeChecker.java
@@ -2,19 +2,39 @@ package week_2_control_flow.day_1_if_else_statements;
 
 public class GradeChecker {
     public static void main(String[] args) {
-
+        
+        // VARIABLE:
+        // stores integer value -> the student's score 
         int score = 85;
 
+        // IF-ELSE LADDER:
+        // used to check multiple conditions in order
+
+        // checks if score is 90 or more
         if (score >= 90) {
-            System.out.println("Garde: A 🌟");
+
+            // runs when the conditions is true
+            System.out.println("Garde: A");
+
+            // checks if score is 80 or more
         } else if (score >= 80) {
-            System.out.println("Grade: B 👍");
+            
+            // runs when the condition is false
+            System.out.println("Grade: B");
+
+            // checks if score is 70 or more
         } else if (score >= 70) {
-            System.out.println("Grade: C 🙂");
+            System.out.println("Grade: C");
+
+            // checks if score is 60 or more
         } else if (score >= 60) {
-            System.out.println("Grade: D ⚠️");
+            
+            // runs when all the condition above is false
+            // runs if none of the above conditions are true
+            System.out.println("Grade: D");
         } else {
-            System.out.println("Grade: F ❌");
+            
+            System.out.println("Grade: F");
         }
     }
 }
