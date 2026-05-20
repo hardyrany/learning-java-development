9db1f23ae4a7a6ca5cacd401af59e1fc50717808
Hardy Fernandes <hardymtf@gmail.com>
2026-05-20 15:24:20 -0100

    docs: nested-if comment

--- src/week_2_control_flow/day_1_if_else_statements/NestedIf.java
+++ src/week_2_control_flow/day_1_if_else_statements/NestedIf.java
@@ -1,18 +1,36 @@
 package week_2_control_flow.day_1_if_else_statements;
 
 public class NestedIf {
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
+        // VARIABLES:
+
+        // stores person's age values
         int age = 20;
+
+        // stores true or false value
+        // true means the person has an ID card
         boolean hasID = true;
 
+        // NESTED IF:
+        // an if statement inside another if statement
+
+        // first condition checks age
         if (age >= 18) {
+
+            // secund condition checks if person has ID
             if (hasID) {
-                System.out.println("Access granted ✅");
+
+                // runs if both condition is true
+                System.out.println("Access granted!");
             } else {
-                System.out.println("Need ID card ⚠️");
+
+                // runs if age is true but no ID is provided
+                System.out.println("Need ID card!");
             }
         } else {
+
+            // runs if age is under 18
             System.out.println("Too young ❌");
         }
     }
