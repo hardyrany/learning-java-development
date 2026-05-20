2ed0b59639720c3b1183ef1c19dbb27945df4d4d
Hardy Fernandes <hardymtf@gmail.com>
2026-05-20 15:21:17 -0100

    docs: ifelse v2 comment

--- src/week_2_control_flow/day_1_if_else_statements/IfElseV2.java
+++ src/week_2_control_flow/day_1_if_else_statements/IfElseV2.java
@@ -1,21 +1,36 @@
 package week_2_control_flow.day_1_if_else_statements;
 
-import java.util.Scanner;
+import java.util.Scanner;   // import Scanner class for user input
 
 public class IfElseV2 {
     public static void main(String [] args) {
-
+        
+        // SCANNER OBJECT:
+        // used to read keyword input
         Scanner scanner = new Scanner(System.in);
 
+        // USER INPUT SECTION
+        // ask user for age
         System.out.println("How old are you?");
+        
+        // read integer input from user
         int age = scanner.nextInt();
 
+        // IF-ELSE STATEMENT:
+        // used to make decisions based on conditions
+
+        // checks if age is 18 or older
         if (age >= 18) {
-            System.out.println("You are an adult ✅");
+
+            // runs when the conditions is true
+            System.out.println("You are an adult!");
         } else {
-            System.out.println("You are an minor ❌");
+            
+            // runs when the condition is false
+            System.out.println("You are an minor !");
         }
 
+        // close scanner to free system resources
         scanner.close();
     }
 }
