8d5ff3dc9db457b0fed19abda5fd1d01499bfdc4
Hardy Fernandes <hardymtf@gmail.com>
2026-05-20 15:25:08 -0100

    docs: ternary comment

--- src/week_2_control_flow/day_1_if_else_statements/Ternary.java
+++ src/week_2_control_flow/day_1_if_else_statements/Ternary.java
@@ -3,14 +3,29 @@ package week_2_control_flow.day_1_if_else_statements;
 public class Ternary {
     public static void main(String[] args) {
 
+        // VARIABLE:
+        // stores integer value -> person's age
         int age = 20;
 
-        // condition ? valueIfTrue : valueIfFalse
-        String result = (age >= 18) ? "Adult" : "Minor";
-        System.out.println(result); // Adult
+        // TERNARY OPERATOR:
+        // short form of if-else statement
 
+        // syntax:
+        // (condition) ? valueIfTrue : valueIfFalse
+        // ? valueIfTrue -> represent true condition
+        // : valueIfFalse -> represent false condition
+        // output is witch one is right
+
+        // checks if age is 18 or more
+        // if age is 18 or more print 'Adult'
+        // if age is under 18 print 'Minor'
+        String result = (age >= 18) ? "Adult" : "Minor";
+        System.out.println(result); // prints Adult
+
+        // TERNARY OPERATOR (MAX VALUE):
+        // checks which number is greater
         int a = 10, b = 20;
         int max = (a > b) ? a : b;
-        System.out.println("Max: " + max); // Max: 20
+        System.out.println("Max: " + max); // prints: Max: 20
     }
 }
